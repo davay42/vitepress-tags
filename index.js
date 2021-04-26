e54f806ebcacfe5f07f331b1111766d6c223228d
davay42 <fond@fondfrukt.ru>
2021-04-26 18:53:48 +0300

    upd

--- index.js
+++ index.js
@@ -3,31 +3,43 @@ const path = require('path')
 const matter = require('gray-matter')
 
 const glob = require('glob')
+const { url } = require('inspector')
 
 module.exports = function (dir = './pages', pattern = '/**/*.md') {
-  const filesList = glob.sync(dir + pattern, { nodir: true })
-  console.log(filesList)
-  const tags = {}
+  const pageDir = path.resolve(__dirname, dir)
 
+  const filesList = glob.sync(pageDir + pattern, { nodir: true })
+  const tags = {}
   const all = filesList.map((file) => {
-    let info = matter(fs.readFileSync(file, 'utf8'))
+    let stats = fs.statSync(file)
+    let fileContent = fs.readFileSync(file, 'utf8')
+    let frontmatter = matter(fileContent)
+    let relLink = path.relative(dir, file)
+    let url = relLink.includes('index.md')
+      ? relLink.slice(0, -8)
+      : relLink.slice(0, -3) + '.html'
+
     let data = {
-      title: info.data?.title,
-      url:
-        dir.split('/').pop() +
-        '/' +
-        path.relative(dir, file).split('.').shift(),
-      data: info?.data,
+      title: frontmatter.data?.title,
+      subtitle: frontmatter.data?.subtitle,
+      text: frontmatter.data?.title,
+      lastModified: stats.mtime,
+      link: '/' + url,
+      data: frontmatter?.data,
+      more: !!frontmatter.content,
     }
 
-    if (typeof info?.data?.tags == 'string') {
-      let tag = info?.data?.tags
+    if (typeof frontmatter?.data?.tags == 'string') {
+      let tag = frontmatter?.data?.tags
       tags[tag] = tags[tag] || []
       tags[tag].push(data)
     }
 
-    if (Array.isArray(info.data?.tags) && info?.data?.tags.length > 0) {
-      info.data.tags.forEach((tag) => {
+    if (
+      Array.isArray(frontmatter.data?.tags) &&
+      frontmatter?.data?.tags.length > 0
+    ) {
+      frontmatter.data.tags.forEach((tag) => {
         tags[tag] = tags[tag] || []
         tags[tag].push(data)
       })
